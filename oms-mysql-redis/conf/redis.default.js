03a3f52648eae48a50302140d7b73d8bced324c5
samxu-tech <75049698@qq.com>
2021-10-25 13:17:14 +0800

    first commit

---
+++ oms-mysql-redis/conf/redis.default.js
@@ -0,0 +1,12 @@
+module.exports = {
+	pluginConfig: {
+		// During development you may set the environment variables in conf/.env
+		// More info at: https://docs.axway.com/bundle/API_Builder_4x_allOS_en/page/environmentalization.html
+		'@axway-api-builder-ext/api-builder-plugin-fn-redis': {			
+			host: process.env.REDIS_HOST || "redis", // Redis server location
+			port: parseInt(process.env.REDIS_PORT) || 6379, // Redis server port
+			password: process.env.REDIS_PASSWORD || "Axway123!", // Password for the Redis connection
+			registerHooks: true // When true register callbacks to be excuted on runtime lifecycle events
+		}
+	}
+};
